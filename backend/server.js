9ce842af89617fa04c48d6165466c8eaf0ec76cb
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-12-22 22:35:41 +0530

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
+import connetToMongoDB from "./db/connectTomongoDB.js";
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res)=>{
-    //root router http://localhost:5000/
-    res.send("Hello World! hi ");
-});
 
+dotenv.config();
+
+ app.use(express.json());
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, ()=> console.log(`Server Running on Port ${PORT}`));
+// app.get("/", (req, res)=>{
+//     //root router http://localhost:5000/
+//     res.send("Hello World! hi ");
+// });
+
+
+app.listen(PORT, ()=>{
+    connetToMongoDB();
+    console.log(`Server Running on Port ${PORT}`)
+});
